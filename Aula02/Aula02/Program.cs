e6650c8f5473f7745246a8a6950628db46091118
WilliamBatista1 <william.batista1996@hotmail.com>
2025-02-25 20:46:25 -0300

    Complemento da aula

--- Aula02/Aula02/Program.cs
+++ Aula02/Aula02/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("William\n");
+Console.WriteLine("Batista");
+
+string firstName = "William";
+string lastName = "Batista";
+
+Console.WriteLine("Meu nome completo é" + firstName + " " + lastName);
+
+Console.WriteLine($"Meu nome ccompleto é "(firstName)(lastName);
