2b7cdbe19298f029105c653c6609f9d87f855e6b
WilliamBatista1 <william.batista1996@hotmail.com>
2025-03-06 22:24:56 -0300

    Retangulo

---
+++ produto/Produto/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Produto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto("Lapis", 500, 81); 
+            Produto produto2 = new Produto("Pacoca", 200, 1); 
+            Produto produto3 = new Produto("Chá Bear Matte", 8, 400); 
+
+            Console.WriteLine(produto1.ToString()); 
+            Console.WriteLine(produto2.ToString()); 
+            Console.WriteLine(produto3.ToString()); 
+
+
+        }
+    }
+}
---
+++ produto/Produto/retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retangulo
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public int Altura;
+
+
+        public double tamanho() { return Largura;}
+ 
+        
+        public double perimetro () {
+
+            return $"Largura, Altura";
+
+
+
+
+        }
+
+    }
+
+}
